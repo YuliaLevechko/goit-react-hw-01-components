165d799779803bb059fcc03d6deff4a33498efae
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2023-09-14 01:35:03 +0300

    added task-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import Profile from './Profile/Profile';
+
+import user from './Data/user.json'
+import data from './Data/data.json'
+import friends from './Data/friends.json'
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile users={user} />
+    </>
   );
 };
